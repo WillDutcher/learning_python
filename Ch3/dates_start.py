cd135e36806be621eb3e3b5aa9c8cd6b11ae1ee8
Will Dutcher <william.c.dutcher@gmail.com>
2018-04-19 21:46:13 -0400

    Completed Lesson 7: Date & Time

--- Ch3/dates_start.py
+++ Ch3/dates_start.py
@@ -1,27 +1,38 @@
 #
 # Example file for working with date information
 #
-
+from datetime import date
+from datetime import time
+from datetime import datetime
 
 def main():
   ## DATE OBJECTS
   # Get today's date from the simple today() method from the date class
-
+  today = date.today()
+  print("Today's date is: ", today)
 
   # print out the date's individual components
+  day = today.day
+  month = today.month
+  year = today.year
+  print("Day: ", day)
+  print("Month: ", month)
+  print("Year: ", year)
 
-  
   # retrieve today's weekday (0=Monday, 6=Sunday)
+  print("Today's weekday number is: ", today.weekday())
+  days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+  print("Which is a ", days[today.weekday()])
 
-  
   ## DATETIME OBJECTS
   # Get today's date from the datetime class
-
+  today = datetime.now()
+  print("The current date and time is:", today)
 
   # Get the current time
+  t = datetime.time(datetime.now())
+  print(t)
 
 
-  
 if __name__ == "__main__":
   main();
-  
