2c488c7468a4fa8c540edf331d08f77561ffa73e
Will Dutcher <william.c.dutcher@gmail.com>
2018-04-21 08:25:07 -0400

    Completed Lesson 14: Read http through URL

--- Ch5/inetdata_start.py
+++ Ch5/inetdata_start.py
@@ -1,7 +1,14 @@
-# 
+#
 # Example file for retrieving data from the internet
 #
 
+import urllib.request # Provides classes and code to make http requests
+
+def main():
+    webUrl = urllib.request.urlopen("http://www.google.com")
+    print("result code: " + str(webUrl.getcode())) # Result code: 200 = everything is okay"
+    data = webUrl.read() # return HTML of the page in question
+    print(data)
 
 if __name__ == "__main__":
   main()
